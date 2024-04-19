07c3e36f15785212007f48c14861e9acd7170462
Simon Uggla <simon.uggla@gmail.com>
2024-04-19 16:03:33 +0200

    Add rate-limit to api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ['nuxt-auth-utils', '@nuxtjs/eslint-module'],
+	modules: ['nuxt-auth-utils', '@nuxtjs/eslint-module', 'nuxt-rate-limit'],
 	runtimeConfig: {
 		db: {
 			dir: './server/database',
