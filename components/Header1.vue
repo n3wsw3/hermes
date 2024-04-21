3cfa50a4291457d5b1120f77c4613f7be8d04a11
Simon Uggla <simon.uggla@gmail.com>
2024-04-21 21:58:21 +0200

    Add standardized headers

---
+++ components/Header1.vue
@@ -0,0 +1,5 @@
+<template>
+	<h1 class="mt-5 text-3xl font-bold pb-2 text-white tracking-tight">
+		<slot></slot>
+	</h1>
+</template>
---
+++ components/Header2.vue
@@ -0,0 +1,5 @@
+<template>
+	<h2 class="mt-5 text-2xl font-bold pb-2 text-white tracking-tight">
+		<slot></slot>
+	</h2>
+</template>
