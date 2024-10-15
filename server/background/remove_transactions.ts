6bf3c16c43ff7a9081768bb33ef3967b409c6eaf
Simon Uggla <simon.uggla@gmail.com>
2024-10-15 15:09:45 +0200

    Formatting

--- server/background/remove_transactions.ts
+++ server/background/remove_transactions.ts
@@ -1,6 +1,6 @@
-import { consola } from "consola";
-import { transactions } from "../database/schema";
-import { and, isNull } from "drizzle-orm";
+import { consola } from 'consola';
+import { transactions } from '../database/schema';
+import { and, isNull } from 'drizzle-orm';
 
 export default {
 	id: 'remove_transactions',
@@ -9,9 +9,9 @@ export default {
 
 		// Remove transactions where both to and from is null
 		const db = useDb();
-		db.transaction(async (tx) => {
+		db.transaction(async tx => {
 			tx.delete(transactions).where(and(isNull(transactions.to), isNull(transactions.from)));
 		});
 	},
 	interval: 1000 * 60 * 60 * 24
-}
+};
